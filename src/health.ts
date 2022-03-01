4ecfdec7ea2d0b26705583709f3ad569b79af95b
Alistair Scott <ali.scott@gmail.com>
2022-03-01 17:24:27 +0000

    enhance: update healthcheck to check database connection

--- src/health.ts
+++ src/health.ts
@@ -1,9 +1,19 @@
 import express from 'express';
+import config from './config';
 
 const router = express.Router();
 
-router.get('/health', (_req, res) =>
-  res.status(200).json({ status: 'success' })
-);
+router.get('/health', async (_req, res) => {
+  try {
+    const pool = await config.pg();
+    const client = await pool.connect();
+    await client.query('SELECT 1');
+  } catch (err) {
+    config.logger.error(`Could not connect to database: ${err}`);
+    res.status(500).json({ status: 'failed' });
+    return;
+  }
+  res.status(200).json({ status: 'success' });
+});
 
 export default router;
